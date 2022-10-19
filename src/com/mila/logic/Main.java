2b2177ee3eb563fa792d0165597750dcb4ef710e
Amanda Fontana Castillo <amanda.fontana@gmail.com>
2022-10-19 13:02:46 +0200

    Added Main class with printout implemented

---
+++ src/com/mila/logic/Main.java
@@ -0,0 +1,14 @@
+package com.mila.logic;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calculator calculator = new Calculator();
+		int addResult = calculator.add(4, 8);
+		
+		System.out.println("El resultat de la suma es " + addResult);
+		
+	}
+
+}
